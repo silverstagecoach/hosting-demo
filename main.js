7f51f40548b60714f9a71c37a4508c59bf45e234
Jonathan McArthur <silverstagecoach@gmail.com>
2022-07-20 15:32:17 -0500

    no message

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
-let button = document.querySelector('button')
+let clickButton = document.querySelector('.click')
+let repeatButton = document.querySelector('.repeat')
+let noButton = document.querySelector('.no')
+let hereButton = document.querySelector('.here')
 
-const buttonAlert = () => {
-    alert('My button worked.')
+const wrongButton = () => {
+    alert('You chose \n\n POORLY')
 }
 
-button.addEventListener('click', buttonAlert)
+const reallyWrongButton = () => {
+    alert('Sorry, this was definitely the wrong button \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
+}
+
+clickButton.addEventListener('click', wrongButton)
+repeatButton.addEventListener('click', wrongButton)
+noButton.addEventListener('click', wrongButton)
+hereButton.addEventListener('hover', reallyWrongButton)
